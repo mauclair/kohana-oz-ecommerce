83d297bdd723fd2933caf42e6a2c78c05ddd2bb1
Alex Cartwright <alexc223@googlemail.com>
2011-09-23 22:08:43 +0100

    Added voucher model messages default text

---
+++ messages/models/voucher.php
@@ -0,0 +1,17 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'code'       => array(
+		'max_length'     => 'Code must be no longer than :param2 characters long',
+		'code_available' => 'The provided code has already been taken',
+	),
+	'start_date' => array(
+		'date' => 'Start date must be a valid date',
+	),
+	'end_date'   => array(
+		'date' => 'End date must be a valid date',
+	),
+	'percentage' => array(
+		'range' => 'Percentage must be between 0 and 100',
+	)
+);
