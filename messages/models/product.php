b8d3bada7c4135f8ff8dde105da717b3dd2dfa46
Alex Cartwright <alex@openzula.org>
2012-02-02 11:33:23 +0000

    Updated messages due to column changes in product & product_variation

--- messages/models/product.php
+++ messages/models/product.php
@@ -7,17 +7,4 @@ return array(
 	'description' => array(
 		'not_empty' => 'Description is required',
 	),
-	'price' => array(
-		'not_empty' => 'Please provide a numeric price',
-		'numeric'   => 'Please provide a numeric price',
-		'gte'       => 'Price must be greater than or equal to :param2',
-	),
-	'sale_price' => array(
-		'numeric' => 'Please provide a numeric sale price',
-		'gte'     => 'Sale price must be greater than or equal to :param2',
-		'lt'	  => 'Sale price must be less than the original price',
-	),
-	'quantity' => array(
-		'digit' => 'Quantity must be a digit',
-	),
 );
--- messages/models/product_variation.php
+++ messages/models/product_variation.php
@@ -4,6 +4,16 @@ return array(
 	'name' => array(
 		'not_empty' => 'Variation name is required',
 	),
+	'price' => array(
+		'not_empty' => 'Please provide a numeric variation price',
+		'numeric'   => 'Please provide a numeric variation price',
+		'gte'       => 'Variation price must be greater than or equal to :param2',
+	),
+	'sale_price' => array(
+		'numeric' => 'Please provide a numeric variation sale price',
+		'gte'     => 'Variation sale price must be greater than or equal to :param2',
+		'lt'	  => 'Variation sale price must be less than the original price',
+	),
 	'quantity' => array(
 		'not_empty' => 'Variation quantity must be a digit',
 		'digit'     => 'Variation quantity must be a digit',
